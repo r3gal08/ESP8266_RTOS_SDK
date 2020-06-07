21b793f6abb29b63af0977e187e7639d061592e1
r3gal08 <brettleard808@gmail.com>
2020-06-07 18:01:05 -0300

    changed to a simple blink program for testing udp

--- examples/peripherals/gpio/main/user_main.c
+++ examples/peripherals/gpio/main/user_main.c
@@ -40,35 +40,16 @@ static const char *TAG = "main";
  *
  */
 
-#define GPIO_OUTPUT_IO_0    15
-#define GPIO_OUTPUT_IO_1    16
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
-#define GPIO_INPUT_IO_0     4
-#define GPIO_INPUT_IO_1     5
-#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+#define GPIO_OUTPUT_IO_power    16
+#define GPIO_OUTPUT_IO_speed    15
+#define GPIO_OUTPUT_IO_rot      14
+#define GPIO_OUTPUT_IO_windMode 13
+#define GPIO_OUTPUT_IO_timer    12
 
-static xQueueHandle gpio_evt_queue = NULL;
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_power) | (1ULL<<GPIO_OUTPUT_IO_speed) | ((1ULL<<GPIO_OUTPUT_IO_rot) | (1ULL<<GPIO_OUTPUT_IO_windMode)) | (1ULL<<GPIO_OUTPUT_IO_timer))
 
-static void gpio_isr_handler(void *arg)
-{
-    uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
-}
-
-static void gpio_task_example(void *arg)
-{
-    uint32_t io_num;
-
-    for (;;) {
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            ESP_LOGI(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
-        }
-    }
-}
-
-void app_main(void)
-{
-    gpio_config_t io_conf;
+void gpio_Init() {
+    gpio_conf ig_t io_conf;
     //disable interrupt
     io_conf.intr_type = GPIO_INTR_DISABLE;
     //set as output mode
@@ -81,44 +62,21 @@ void app_main(void)
     io_conf.pull_up_en = 0;
     //configure GPIO with the given settings
     gpio_config(&io_conf);
+}
 
-    //interrupt of rising edge
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    //bit mask of the pins, use GPIO4/5 here
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    //set as input mode
-    io_conf.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
 
-    //change gpio intrrupt type for one pin
-    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
-
-    //create a queue to handle gpio event from isr
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    //start gpio task
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
-
-    //install gpio isr service
-    gpio_install_isr_service(0);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *) GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void *) GPIO_INPUT_IO_1);
-
-    //remove isr handler for gpio number.
-    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin again
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *) GPIO_INPUT_IO_0);
+void app_main(void) {
+    gpio_Init();
 
     int cnt = 0;
-
     while (1) {
         ESP_LOGI(TAG, "cnt: %d\n", cnt++);
         vTaskDelay(1000 / portTICK_RATE_MS);
-        gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
-        gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
+        gpio_set_level(GPIO_OUTPUT_IO_power, cnt%2);
+        gpio_set_level(GPIO_OUTPUT_IO_speed, cnt%2);
+        gpio_set_level(GPIO_OUTPUT_IO_rot, cnt%2);
+        gpio_set_level(GPIO_OUTPUT_IO_windMode, cnt%2);
+        gpio_set_level(GPIO_OUTPUT_IO_timer, cnt%2);
     }
 }
 
